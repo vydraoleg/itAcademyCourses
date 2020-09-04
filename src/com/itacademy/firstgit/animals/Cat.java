d677710cb32bca643315db0066cb6b557eae69c0
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-09-04 21:18:17 +0300

    4444

--- src/com/itacademy/firstgit/animals/Cat.java
+++ src/com/itacademy/firstgit/animals/Cat.java
@@ -1,11 +1,17 @@
 package com.itacademy.firstgit.animals;
 
-public class Cat extends Animal{
+public class Cat extends Animal implements ISaySomething{
+
     @Override
     public void saySmth(){
         System.out.println("say MEOW");
     }
     public void  catchMouse(){
         System.out.println("Got the mouse!");
+    }
+
+    @Override
+    public void play(){
+        System.out.println("Cat is playing");
     }
 }
---
+++ src/com/itacademy/firstgit/animals/ISaySomething.java
@@ -0,0 +1,6 @@
+package com.itacademy.firstgit.animals;
+
+public interface ISaySomething {
+    // Все методы без реализации и публичные
+    void play();
+}
