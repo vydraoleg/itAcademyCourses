03ebdbe86544022a026c04ba6338ee6160038f59
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-09-04 16:12:32 +0300

    Update for decrease increment
    Update for decrease increment

--- src/com/itacademy/firstgit/Main.java
+++ src/com/itacademy/firstgit/Main.java
@@ -1,12 +1,28 @@
 package com.itacademy.firstgit;
 
-//import com.itacademy.firstgit.animals.Cat;
+import com.itacademy.firstgit.animals.Cat;
+import com.itacademy.firstgit.animals.Dog;
 import com.itacademy.firstgit.animals.Pig;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        Pig pig= new Pig("Borka");
 
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+
+		Cat vaska = new Cat();
+		vaska.setAge(5);
+		vaska.setName("Vaska");
+
+		Dog sharik = new Dog();
+		sharik.setAge(4);
+		sharik.setName("Sharik");
+
+		Pig borka = new Pig();
+		borka.setAge(4);
+		borka.setName("Borka");
+
+		System.out.println("Cat's name is " + vaska.getName());
+		System.out.println("Dog's name is " + sharik.getName());
+		System.out.println("Pig's name is " + borka.getName());
+	}
 }
--- src/com/itacademy/firstgit/animals/Dog.java
+++ src/com/itacademy/firstgit/animals/Dog.java
@@ -1,4 +1,23 @@
 package com.itacademy.firstgit.animals;
 
 public class Dog {
+
+	private String name;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
