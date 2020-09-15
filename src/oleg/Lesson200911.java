7a0f15399d89624f81517f18287857db7277b819
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-09-15 22:07:48 +0300

    Работа дома ListArray

--- src/oleg/Lesson200911.java
+++ src/oleg/Lesson200911.java
@@ -1,6 +1,7 @@
 package oleg;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,6 +11,24 @@ public class Lesson200911 {
          LinkedList list1 = new LinkedList(); // каждый элемент + хранит ссылку на next and previous element
          ArrayList list2 = new ArrayList(); // динамический массив ЧАЩЕ
         // SingleToneList - хранит одно значение
-         List<Number> list = new ArrayList<>();
+         List<Number> list = new ArrayList<>(){
+             {
+                 add(100);
+                 add(150);
+                 add(80);
+             }
+         };
+         for (int i = 0; i<20;i++){
+             list.add((int)(Math.random()*100));
+         }
+         for(Number ind : list){
+             System.out.println(ind);
+         }
+        Iterator<Number> it = list.iterator();
+        Integer res;
+        while ( it.hasNext()){
+            res = (int)it.next() ;
+            System.out.println(res);
+        }
     }
 }
