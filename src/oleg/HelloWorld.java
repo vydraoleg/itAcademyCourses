f4f625954febf28b0a92501aa7efa5ffac82e01c
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-09-16 16:31:35 +0300

    111

--- src/oleg/HelloWorld.java
+++ src/oleg/HelloWorld.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HelloWorld {
-    private static int i =050;
-    public enum SmtnConst { i}
+    private static int i = 050;
+
+    public enum SmtnConst {i}
 
     public static void main(String[] args) {
-        StringBuilder j= new StringBuilder(" Addition");
+        StringBuilder j = new StringBuilder(" Addition");
         System.out.println(+i);
-        System.out.println("I \"like\" Java! "+ j);
+        System.out.println("I \"like\" Java! " + j);
         boolean rt = true;
-        System.out.println("I like Java! "+rt);
+        System.out.println("I like Java! " + rt);
 
         String[] array = new String[5];
 // Generic Collection
