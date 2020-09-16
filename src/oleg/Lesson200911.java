676a86fabe809358cc19df4c3ad324dc3651c774
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-09-16 17:36:53 +0300

    Second homework

--- src/oleg/Lesson200911.java
+++ src/oleg/Lesson200911.java
@@ -1,34 +1,42 @@
 package oleg;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Lesson200911 {
     public static void main(String[] args) {
         // основные
-         LinkedList list1 = new LinkedList(); // каждый элемент + хранит ссылку на next and previous element
-         ArrayList list2 = new ArrayList(); // динамический массив ЧАЩЕ
+        LinkedList list1 = new LinkedList(); // каждый элемент + хранит ссылку на next and previous element
+        ArrayList list2 = new ArrayList(); // динамический массив ЧАЩЕ
         // SingleToneList - хранит одно значение
-         List<Integer> list = new ArrayList<Integer>(){
-             {
-                 add(100);
-                 add(150);
-                 add(80);
-             }
-         };
-         for (int i = 0; i<20;i++){
-             list.add((int)(Math.random()*100));
-         }
-         for(Number ind : list){
-             System.out.println(ind);
-         }
+        List<Integer> list = new ArrayList<Integer>() {
+            {
+                add(100);
+                add(150);
+                add(80);
+            }
+        };
+        for (int i = 0; i < 7; i++) {
+            list.add((int) (Math.random() * 100));
+        }
+
+        for (Number ind : list) {
+            System.out.print(ind+" ");
+        }
+        System.out.println(" ");
+
+        Collections.shuffle(list);
+        for (Number ind : list) {
+            System.out.print(ind+" ");
+        }
+        System.out.println(" ");
+        Collections.sort(list);
+
         Iterator<Integer> it = list.iterator();
         Integer res;
-        while ( it.hasNext()){
-            res = (int)it.next() ;
-            System.out.println(res);
+        while (it.hasNext()) {
+            res = (int) it.next();
+            System.out.print(res+" ");
         }
+        System.out.println(" ");
     }
 }
