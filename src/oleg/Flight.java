0c54021102db4963b6f834d14191e117cbe6ffba
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-09-22 16:09:56 +0300

    11111111111

---
+++ src/oleg/Flight.java
@@ -0,0 +1,11 @@
+package oleg;
+
+    public class Flight {
+        private FlightNumber number;
+
+        public Flight(FlightNumber number) {
+            this.number = number;
+        }
+    }
+
+
---
+++ src/oleg/FlightNumber.java
@@ -0,0 +1,6 @@
+package oleg;
+
+public class FlightNumber {
+    public int value;
+    public String code;
+}
