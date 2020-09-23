2bfce91003537056c5096c4897b323e1ef6f7e79
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-09-23 19:05:40 +0300

    3

--- src/oleg/Flight.java
+++ src/oleg/Flight.java
@@ -1,11 +1,20 @@
 package oleg;
 
-    public class Flight {
-        private FlightNumber number;
-
-        public Flight(FlightNumber number) {
-            this.number = number;
+public class Flight {
+    public static void main(String[] args) {
+        final int score1 = 8, score2 = 3;
+        char myScore = 7;
+        var e =0;
+        switch (myScore) {
+            default:
+                case score1:
+                case 2: case 6:System.out.print("great-");
+                case 4:System.out.print("good-");
+                break;
+            case score2:
+            case 1:System.out.print("not good-");
         }
     }
+}
 
 
