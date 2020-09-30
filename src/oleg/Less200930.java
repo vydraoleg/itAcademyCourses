dfdde865dc03be3b3a770daf92564b14ed476892
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-09-30 21:25:24 +0300

    22222
    222222222

--- src/oleg/Less200930.java
+++ src/oleg/Less200930.java
@@ -3,12 +3,31 @@ package oleg;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+//import java.util.Calendar; @Deprecated
+//import java.util.Date;  @Deprecated
+//import java.sql.Date;@Deprecated
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Less200930 {
+    public enum TypeOfAnimals{
+        CAT, DOG, MONKEY
+    }
+    @Deprecated
+    private Less200930(){}  // не даст создать класс
+
     public static void main(String[] args) {
-//        System.out.println(" 3333333");
+        LocalDate date = LocalDate.ofYearDay(2020, 256);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date1 = LocalDate.parse("2020-01-20",formatter);
+        System.out.println(date1);
+
+
         try {
             Map<String, String> lines = Files.lines(Paths.get("d:\\oleg\\txt.txt"))
                     .collect(Collectors.toMap(x -> x, y -> y));
