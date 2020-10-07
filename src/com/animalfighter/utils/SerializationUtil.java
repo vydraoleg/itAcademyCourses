dd479127ff6b72b4aaee994b957b971cf8417e95
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-10-07 21:22:00 +0300

    last 111 1
    last 1111

--- src/com/animalfighter/utils/SerializationUtil.java
+++ src/com/animalfighter/utils/SerializationUtil.java
@@ -1,7 +1,38 @@
 package com.animalfighter.utils;
 
+import com.animalfighter.entities.Animal;
+
+import java.io.*;
+//"..\\txt1.txt"
 public class SerializationUtil {
-    public boolean serialize() {
+    public boolean serialize(Animal animal,String nFile) {
+
+        try (ObjectOutputStream objectOutputStream
+                     = new ObjectOutputStream(new FileOutputStream(nFile))){
+            System.out.println(animal.toString());
+            objectOutputStream.writeObject(animal);
+            System.out.println("person was serialized");
+        } catch (IOException e) {   // FileNotFoundException |
+            e.printStackTrace();
+            return false;
+
+        }
+
+        System.out.println("person current");
+        System.out.println(animal.toString());
         return true;
     }
+//try with resources  ;
+
+    public Animal deserialize(String nFile) {
+        try (ObjectInputStream objectInputStream
+                     = new ObjectInputStream(new FileInputStream(nFile))){
+            return  (Animal) objectInputStream.readObject();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
