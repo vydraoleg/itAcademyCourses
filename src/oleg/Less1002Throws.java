764752792e72f39b361c7fef63ec539c48f2332a
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-10-09 18:57:22 +0300

    111111111
    11111111111111

--- src/oleg/Less1002Throws.java
+++ src/oleg/Less1002Throws.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 //import lombok.*;
 
 //@Slf4j2
-public class Less1002Thread {
+public class Less1002Throws {
     public static void main(String[] args) throws IOException {
         Cat cat = new Cat();
           cat.setName("Vaska");
