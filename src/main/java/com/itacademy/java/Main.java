ba5cf79a418356e428e7dc5f1baa74a50d9d3704
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-10-16 16:30:17 +0300

    1111111111

--- src/main/java/com/itacademy/java/Main.java
+++ src/main/java/com/itacademy/java/Main.java
@@ -1,8 +1,8 @@
 package com.itacademy.java;
 
-import lombok.Data;
+//import lombok.Data;
 
-@Data
+//@Data
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
