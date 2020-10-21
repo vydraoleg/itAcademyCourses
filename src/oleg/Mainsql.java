6d99e8df66a8d2f63eefe7ae33e3156d64837295
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-10-21 21:10:03 +0300

    222222222
    222222222222222

---
+++ src/oleg/Mainsql.java
@@ -0,0 +1,33 @@
+package oleg;
+
+import java.sql.*;
+
+public class Mainsql {
+
+//        static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        static final String DATABASE_URL = "jdbc:mysql://localhost:3306/oleg_schema";
+
+        public static void main(String[] args) throws SQLException, ClassNotFoundException {
+            Connection connection = null;
+            Statement statement = null;
+
+//            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DATABASE_URL, "root", "root");
+            statement = connection.createStatement();
+            String sql= "SELECT * FROM user;";
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while(resultSet.next()){
+                System.out.println(resultSet.getInt("id"));
+                System.out.println(resultSet.getInt("name"));
+            }
+            resultSet.close();
+            connection.close();
+            statement.close();
+
+        }
+    }
+
+
+
