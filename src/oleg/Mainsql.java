d90fd8c29bf518e6e521a9ecbb17ee1f3ccc93a2
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-10-22 21:34:15 +0300

    222222

--- src/oleg/Mainsql.java
+++ src/oleg/Mainsql.java
@@ -5,22 +5,22 @@ import java.sql.*;
 public class Mainsql {
 
 //        static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-        static final String DATABASE_URL = "jdbc:mysql://localhost:3306/oleg_schema";
+        static final String DATABASE_URL = "jdbc:mysql://localhost:3306/itacademia?useUnicode=true&serverTimezone=UTC";
 
-        public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        public static void main(String[] args) throws SQLException {
             Connection connection = null;
             Statement statement = null;
 
 //            Class.forName(JDBC_DRIVER);
             connection = DriverManager.getConnection(DATABASE_URL, "root", "root");
             statement = connection.createStatement();
-            String sql= "SELECT * FROM user;";
+            String sql= "SELECT * FROM pc";
 
             ResultSet resultSet = statement.executeQuery(sql);
 
             while(resultSet.next()){
-                System.out.println(resultSet.getInt("id"));
-                System.out.println(resultSet.getInt("name"));
+                System.out.println(resultSet.getInt("code"));
+                System.out.println(resultSet.getString("model"));
             }
             resultSet.close();
             connection.close();
