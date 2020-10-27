de495b8222ab1029493ede2dd07f5c7ffd0aa5f0
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-10-27 22:14:28 +0300

    2222222222222222

--- src/oleg/ReflectionAPI.java
+++ src/oleg/ReflectionAPI.java
@@ -1,6 +1,6 @@
 package oleg;
 
-import com.itacademy.firstgit.animals.Cat;
+import com.entity.Pat;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -10,15 +10,15 @@ import java.lang.reflect.Method;
 
 public class ReflectionAPI {
     public static void main(String[] args) {
-//        Cat cat = new Cat(1,"11");
+        Pat pat = new Pat(1, "Tobzik");
 //        int age = cat.getAge();
-        Cat cat = null;
-        Cat cat1 = null;
-        String nameClass=Cat.class.getName();
-//        cat.getClass().getName()
+//        Pat pat = null;
+        Pat pat1 = null;
+        String nameClass = Pat.class.getName();
+//        pat.getClass().getName()
 //        Class clazz = cat.getClass();
 
-
+// получаем ссылку на класс и достпу к private constructor
         try {
             Class<?> clazz2 = Class.forName(nameClass);  // "com.catfighter.entities.Cat"
             Class<?>[] parameters = {int.class, String.class};
@@ -26,35 +26,40 @@ public class ReflectionAPI {
             Constructor<?> constructor = clazz2.getDeclaredConstructor(parameters);
             constructor.setAccessible(true);
 
-            cat1 = (Cat)clazz2.getConstructor(parameters).newInstance(4,"Vaska");
-            Cat cat2 = (Cat) clazz2.getConstructor(int.class, String.class).newInstance(4,"Vaska");
+            pat1 = (Pat) clazz2.getConstructor(parameters).newInstance(4, "Vaska");
+            Pat cat2 = (Pat) clazz2.getConstructor(int.class, String.class).newInstance(4, "Vaska");
 //            cat1 = (Cat)clazz2.newInstance();
         } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
             e.printStackTrace();
         }
-        Class clazz3 = Cat.class;
+        Class clazz3 = Pat.class;
+
 
         //        Class clazz4 = ReflectionAPI.class;
 //        clazz3.getDeclaredFields();
 //        clazz3.getDeclaredMethods();
-        String name="";
+        String name = "";
+//
         try {
 //            Field[] fields = cat.getClass().getDeclaredFields();
-            Field field = cat.getClass().getDeclaredField("name");
+            Field field = pat.getClass().getDeclaredField("name");
             field.setAccessible(true);
-            name = (String) field.get(cat);
+            name = (String) field.get(pat);
         } catch (IllegalAccessException | NoSuchFieldException e) {
             e.printStackTrace();
         }
-        System.out.printf("Cat name is %s ",name);
+        System.out.printf("Cat name is %s \n", name);
 
         String stop = "stop";
         try {
-            Method method = cat.getClass().getDeclaredMethod("printCat");
+            Method method = pat.getClass().getDeclaredMethod("printPat");
             method.setAccessible(true);
-            method.invoke(cat);
+            method.invoke(pat);
         } catch (NoSuchMethodException|IllegalAccessException|InvocationTargetException e) {
+//        } catch (NoSuchMethodException e) {
             e.printStackTrace();
         }
+
     }
+
 }
