3e4666c685cc18659939ff5bd0a891be19a9e1ab
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-12-27 20:19:39 +0300

    212

---
+++ api-module/src/main/java/com/fight/api/servcies/IAnimalService.java
@@ -0,0 +1,20 @@
+package com.fight.api.servcies;
+
+
+import com.fight.api.entities.Animal;
+
+import java.util.List;
+
+public interface IAnimalService {
+
+    public void readAnimals();
+
+    List<Animal> getAnimals();
+
+    void addAnimal(Animal animal);
+
+    Animal getByName(String name);
+
+    void updateAnimalStrength(String name, int strength);
+
+}
