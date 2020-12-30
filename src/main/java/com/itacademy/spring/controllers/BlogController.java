d69b9e096a99e44667f3d843ace0aaffd1324955
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2020-12-30 21:55:55 +0300

    1212

---
+++ src/main/java/com/itacademy/spring/controllers/BlogController.java
@@ -0,0 +1,15 @@
+package com.itacademy.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+
+    @GetMapping("/blog")
+    public String blogMain(Model model){
+        return "blog-main";
+    }
+
+}
