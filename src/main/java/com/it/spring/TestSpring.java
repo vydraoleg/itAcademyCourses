8191c9011e553eb0dfad53ca537afaa54f991478
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2021-01-09 20:01:07 +0300

    123

---
+++ src/main/java/com/it/spring/TestSpring.java
@@ -0,0 +1,13 @@
+package com.it.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml");
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
+    }
+}
