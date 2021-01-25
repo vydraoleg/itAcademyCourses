6635d32d5c657c985e610c983b2918538d331b9b
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2021-01-25 22:17:09 +0300

    21212

--- src/main/java/com/it/spring/ClassicalMusic.java
+++ src/main/java/com/it/spring/ClassicalMusic.java
@@ -1,12 +1,15 @@
 package com.it.spring;
 
 public class ClassicalMusic implements Music {
-    public void doMyInit(){
-        System.out.println("Doing my initialisation");
+    private ClassicalMusic(){}
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
     }
+    public void doMyInit(){ System.out.println("Doing my initialisation"); }
     public void doMyDestroy(){
         System.out.println("Doing my destraction");
     }
+
     @Override
     public String getSong() {
         return "Hungarian rhapsody";
