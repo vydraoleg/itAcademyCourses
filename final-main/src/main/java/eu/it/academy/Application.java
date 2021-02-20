dba65ad0681c6313e67b1d143e15ad03fb3fbb7e
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2021-02-20 22:09:16 +0300

    12131

--- final-main/src/main/java/eu/it/academy/Application.java
+++ final-main/src/main/java/eu/it/academy/Application.java
@@ -3,6 +3,9 @@ package eu.it.academy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @SpringBootApplication
 public class Application {
 
