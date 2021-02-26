294952499d120c1a1c0e1c64732b6eef01b4ff07
Oleg <vydraoleg@gmail.com>
2021-02-26 21:17:56 +0300

    last
    last

--- final-rest/src/main/java/eu/it/academy/rest/MainController.java
+++ final-rest/src/main/java/eu/it/academy/rest/MainController.java
@@ -1,7 +1,11 @@
 package eu.it.academy.rest;
 
+import eu.it.academy.api.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -10,6 +14,12 @@ public class MainController {
     public String home(){
         return "index";
     }
+
+    @PostMapping
+    public ModelAndView createUser(@RequestBody UserDto user) {
+        return null; //this.userService.createUser(user);
+    }
+
     @GetMapping("/admin")
     public String admin(){
         return "admin";
